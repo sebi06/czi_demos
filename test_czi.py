d58267c433fb11727289bd1d690869af5121e8b6
sebi06 <sebrhode@gmail.com>
2020-05-23 21:28:20 +0200

    updates

--- test_czi.py
+++ test_czi.py
@@ -4,7 +4,9 @@ from aicspylibczi import CziFile
 import czifile as zis
 import xmltodict
 
-filename = r'/datadisk1/tuxedo/temp/input/WP96_T=3_Z=4_Ch=2_3x3_A4-A5.czi'
+# filename = r'/datadisk1/tuxedo/temp/input/WP96_T=3_Z=4_Ch=2_3x3_A4-A5.czi'
+# filename = r'/datadisk1/tuxedo/temp/input/WP96_T=3_Z=4_Ch=2_5x5_A4.czi'
+filename = r'/datadisk1/tuxedo/temp/input/DTScan_ID4.czi'
 
 print('----------czifile array ----------')
 czi_czifile_array, md, addmd = imf.get_array_czi(filename, remove_HDim=False)
@@ -41,11 +43,26 @@ print('------------- aicsimageio -------------')
 print('Axes_aics', md['Axes_aics'])
 print('Shape_aics', md['Shape_aics'])
 
+"""
 # Mosaic files ignore the S dimension and use an internal mIndex to reconstruct, the scale factor allows one to generate a manageable image
 czi_aics_mosaicsize = czi_aics.read_mosaic_size()
+czi_aics_mosaic_array = czi_aics.read_mosaic(C=0, scalefactor=1.0)
 print('czi_aics_mosaicsize', czi_aics_mosaicsize)
+print('czi_aics_mosaic_array', czi_aics_mosaic_array.shape)
 
 czi_aics_scenebb = czi_aics.scene_bounding_box()
 czi_aics_scenehw = czi_aics.scene_height_by_width()
 print('czi_aics_scenebb', czi_aics_scenebb)
 print('czi_aics_scenehw', czi_aics_scenehw)
+"""
+
+dims_dict, dimindex_list, numvalid_dims = imf.get_dimorder(md['dimorder_aicspylibczi'])
+
+print(dims_dict)
+print(dimindex_list)
+
+a = [x for x in dimindex_list if x >= 0]
+new_dict = {key: val for key, val in dims_dict.items() if val >= 0}
+
+print(a)
+print(new_dict)
