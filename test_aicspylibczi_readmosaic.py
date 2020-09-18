ea44b811309b3eea9e88a1c437ea9b2ae072c360
Rhode <sebastian.rhode@zeiss.com>
2020-09-18 09:20:11 +0200

    updates

--- test_aicspylibczi_readmosaic.py
+++ test_aicspylibczi_readmosaic.py
@@ -16,10 +16,13 @@ plt.switch_backend('Qt5Agg')
 # filename = r'/datadisk1/tuxedo/temp/input/WP96_T=3_Z=4_Ch=2_5x5_A4.czi'
 #filename = r'/datadisk1/tuxedo/temp/input/DTScan_ID4.czi'
 #filename = r"C:\Temp\input\DTScan_ID4.czi"
-filename = r"C:\Users\m1srh\Downloads\New version with alpha channel to avoid tile dissaperance2.czi"
+#filename = r"C:\Users\m1srh\Downloads\New version with alpha channel to avoid tile dissaperance2.czi"
+#filename = r"D:\ImageData\BrainSlide\DTScan_ID2.czi"
 # filename = r"C:\Users\m1srh\OneDrive - Carl Zeiss AG\Testdata_Zeiss\Atomic\Nuclei\nuclei_RGB\H&E\Tumor_H+E_small2.czi"
 #filename = r"C:\Users\m1srh\OneDrive - Carl Zeiss AG\Testdata_Zeiss\Atomic\Nuclei\nuclei_RGB\H&E\Tumor_H+E.czi"
 #filename = r'D:\OneDrive - Carl Zeiss AG\Testdata_Zeiss\Atomic\Nuclei\nuclei_RGB\H&E\Tumor_H+E.czi'
+#filename = r"D:\ImageData\Castor\Z-Stack_DCV\CellDivision_T=10_Z=15_CH=2_DCV_small.czi"
+filename = r'testdata\WP96_2Pos_B2+B4_S=2_T=2_Z=4_C=3_X=512_Y=256.czi'
 
 # get the metadata from the czi file
 md, addmd = imf.get_metadata(filename)
@@ -55,14 +58,29 @@ print('CZI Size       : ', czi.size)
 print('CZI IsMosaic   : ', czi.is_mosaic())
 print('CZI Scene Shape consistent : ', czi.shape_is_consistent)
 
+if md['ImageType'] == 'czi':
+    isCZI = True
+else:
+    isCZI = False
+
+if md['isRGB']:
+    isRGB= True
+else:
+    isRGB = False
+
+if md['czi_ismosaic']:
+    isMosaic = True
+else:
+    isMosaic = False
+
 
 #output = czi.read_image(S=0, C=0)
 #image = output[0]
 #image_dims = output[1]
 
-mosaic_data = czi.read_mosaic(C=0, scale_factor=1.0)
+mosaic_data = czi.read_mosaic(C=0, Z=0, scale_factor=0.2)
 image2d = np.squeeze(mosaic_data)
-image2d = np.moveaxis(image2d, 0, -1)
+#image2d = np.moveaxis(image2d, 0, -1)
 # convert ZEN BGR into RGB
 image2d = image2d[..., ::-1]
 
